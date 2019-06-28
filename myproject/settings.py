dc440c93e41b368c3c96a937d62814e2dcff4d2b
naowal <way.serongang@gmail.com>
2019-06-29 06:58:31 +0700

    Change DB connect

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'unclebookdb',
+        'USER': 'unclebookdbman',
+        'PASSWORD': 'UncleBook1397',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
